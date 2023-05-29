c68b2336de1883ee280aa495487a17895227ed58
kleberjúnior <101169391+Criadocf@users.noreply.github.com>
2023-05-29 15:54:03 -0300

    Column, função como parâmetro do ElevatedButton

--- main.dart
+++ main.dart
@@ -5,11 +5,43 @@ void main(){
 }
 
 class PerguntaApp extends StatelessWidget {
+  
+  void respostas(){
+    print('PERGUNTA RESPONDIDA');
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    final perguntas = [
+    'Qual seu animal favorito?',
+    'Qual a sua cor favorita?'
+
+  ];
+
     return MaterialApp(
-      home: Text('Olá FLutter'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+           Text(perguntas[0]),
+           ElevatedButton(
+            child: Text('Resposta1'),
+            onPressed: respostas,
+           ),
+           ElevatedButton(
+            child: Text('Resposta2'),
+            onPressed: respostas,
+           ),
+           ElevatedButton(
+            child: Text('Resposta3'),
+            onPressed: respostas,
+           )
+          ],
+        ),
+      )
     );
   }
 }
